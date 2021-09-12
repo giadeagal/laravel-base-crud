a9fed494f510a26c04a74e014fcfc25d3301b562
Gianluca <gl.galassini@gmail.com>
2021-09-12 14:51:37 +0200

    badges e grafica show

--- laravel/resources/views/comics/show.blade.php
+++ laravel/resources/views/comics/show.blade.php
@@ -6,17 +6,30 @@
     <div class="container">
 
         <div class="row">
-            <div class="col">
-                <h1>{{ $comic->title }}</h1>
+            <div class="col text-center">
+                <h1 class="py-5">{{ $comic->title }}</h1>
             </div>
         </div>
 
         <div class="row">
             <div class="col">
-                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}" title="{{ $comic->title }}">
+                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}" >
             </div>
             <div class="col">
                 <p>{!! $comic->description !!}</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col mt-2 mb-5">
+                <div class="badge bg-primary">
+                    {{ $comic->type}}
+                </div>
+                
+                <div class="badge bg-success" title="{{ $comic->title }}">
+                    {{ $comic->price . " €" }}
+                </div>
+
             </div>
         </div>
     </div>
